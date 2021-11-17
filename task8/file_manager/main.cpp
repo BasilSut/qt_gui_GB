b169784762a6cb5f6ff45a71b5ae6c57c729931b
Basil Sut <1vasilisk12@gmail.com>
2021-11-17 08:04:06 +0300

    task 8 file manager+finder

---
+++ task8/file_manager/main.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "text_editor_07_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+
+
+     QObject::connect(&w, SIGNAL(About_to_Q()), &a, SLOT(quit()));
+    return a.exec();
+}
